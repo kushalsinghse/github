e4cb22007986246672192aa337f37e567b7d2d6b
kushalsinghse <kushalsinghse@gmail.com>
2020-10-19 22:28:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+alert("your information");
+
+function ans() {
+	var name = prompt("what is your name ?");
+	document.getElementById("happy").innerHTML= name;
+}
+function bns() {
+	var middle = prompt("what is your middle name singh,kumar,reddy,etc?");
+	document.getElementById("good").innerHTML= middle;
+}
+function vns() {
+	var lastName = prompt("what is your last name ?");
+	document.getElementById("very").innerHTML= lastName;
+}
+function hns() {
+	var age = prompt("age?");
+	document.getElementById("oboma").innerHTML= age;
+}
+
+
+
